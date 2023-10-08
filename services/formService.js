55b3bf9389584d6010ff0cd71c799dc3c8d530e7
IchaPuspitaSari <ichapsptasari@gmail.com>
2023-10-09 00:20:36 +0700

    update

--- services/formService.js
+++ services/formService.js
@@ -11,7 +11,7 @@ const getAllForm = async () => {
   }
 };
 
-//Post
+// Post
 const createForm = async (form) => {
   try {
     const createdForm = await prisma.form.create({
@@ -21,11 +21,20 @@ const createForm = async (form) => {
         message: form.message,
       },
     });
+
+    // Menampilkan log ketika pesan berhasil terkirim
+    console.log("Message sent successfully!");
+
     return createdForm;
   } catch (error) {
     console.error(error);
+
+    // Menampilkan pesan kesalahan ke console
+    console.log("Error in createForm:", error);
+
     return error;
   }
 };
+
 
 module.exports = { createForm, getAllForm };
